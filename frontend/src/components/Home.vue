10f198c18ed08fb4165a052ab1a684069b0d252d
Benjamin Bini <benjamin.bini@sully-group.fr>
2021-01-29 23:09:14 +0100

    Better home

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,43 +1,78 @@
 <template>
   <div
-    class="w-full space-y-6 sm:space-y-0 sm:space-x-6 sm:flex lg:w-1/2"
-    v-if="season.length &gt; 0"
+    class="grid w-full grid-cols-1 space-y-6 lg:grid-cols-3 xl:grid-cols-4 lg:space-y-0 lg:space-x-6"
   >
-    <div class="w-full sm:w-1/2">
+    <div class="xl:col-span-2">
+      <tailwind-card
+        :title="`Dernière session ${session ? ' - ' + formattedDate : ''}`"
+        :to="session ? `/sessions/${session.sessionId}` : ''"
+      >
+        <session-result-chart-container
+          :loading="loading"
+          :results="session ? session.playerResults : []"
+        ></session-result-chart-container>
+      </tailwind-card>
+    </div>
+    <div>
+      <h2 class="pb-0 text-xl font-medium tracking-wide text-white"></h2>
       <RankingComponent
+        :loading="loading"
+        :rankings="rankings"
+        :season-ranking="true"
+        :title="session ? 'Classement' : `Classement ${season}`"
+      ></RankingComponent>
+    </div>
+    <div>
+      <RankingComponent
+        :loading="loading"
         :rankings="rankings"
         :season-ranking="false"
         title="Classement général"
-      ></RankingComponent>
-    </div>
-    <div class="w-full sm:w-1/2">
-      <h2 class="pb-0 text-xl font-medium tracking-wide text-white"></h2>
-      <RankingComponent
-        :rankings="rankings"
-        :season-ranking="true"
-        :title="`Classement ${season}`"
       ></RankingComponent>
     </div>
   </div>
 </template>
 
 <script>
+import { format } from "date-fns";
+import { fr } from "date-fns/locale";
+import SessionResultChartContainer from "@/components/session/SessionResultChartContainer.vue";
 import SessionService from "@/services/session-service";
 import RankingComponent from "@/components/ranking/RankingComponent";
+import TailwindCard from "@/components/ui/TailwindCard.vue";
 
 export default {
   name: "Home",
   components: {
     RankingComponent,
+    SessionResultChartContainer,
+    TailwindCard,
   },
   data: () => ({
     rankings: Array,
     season: "",
+    session: undefined,
+    loading: true,
   }),
+  computed: {
+    formattedDate: function () {
+      if (!this.session) return;
+      const formattedDate = format(
+        new Date(this.session.date),
+        "EEEE d MMMM yyyy",
+        {
+          locale: fr,
+        }
+      );
+      return formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1);
+    },
+  },
   mounted: async function () {
     const response = await SessionService.getLatestSession();
+    this.session = response.data;
     this.season = response.data.season.name;
     this.rankings = [...response.data.rankings];
+    this.loading = false;
   },
 };
 </script>
--- frontend/src/components/ui/TailwindCard.vue
+++ frontend/src/components/ui/TailwindCard.vue
@@ -3,7 +3,12 @@
     class="overflow-hidden bg-white divide-y divide-gray-200 rounded-lg shadow"
   >
     <h3 class="px-6 py-5 text-xl font-bold leading-6 text-indigo-600">
-      {{ title }}
+      <router-link v-if="to" :to="to">
+        {{ title }}
+      </router-link>
+      <template v-if="!to">
+        {{ title }}
+      </template>
     </h3>
     <div class="px-4 py-5 sm:p-6">
       <slot></slot>
@@ -14,6 +19,6 @@
 <script>
 export default {
   name: "TailwindCard",
-  props: ["title"],
+  props: ["title", "to"],
 };
 </script>
